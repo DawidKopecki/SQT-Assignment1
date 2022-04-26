9fae44ffa18c048a9809ff3060305bc0ad4c7cd1
DawidKopecki <78148257+DawidKopecki@users.noreply.github.com>
2022-04-26 22:50:47 +0100

    Add project files.

---
+++ SQT-Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SQT_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class InsuranceService
+    {
+        public float CalcPremium(int age, string location)
+        {
+            double premium;
+            if (location == "rural")
+                if ((age >= 18) && (age <= 30))
+                    premium = 5.0;
+                else
+                if (age >= 31)
+                    premium = 2.50;
+                else
+                    premium = 0.0;
+            else
+                if (location == "urban")
+                    if ((age >= 18) && (age <= 35))
+                        premium = 6.0;
+                    else
+                    if (age >= 36)
+                        premium = 5.0;
+                    else
+                        premium = 0.0;
+                else
+                    premium = 0.0;
+            if (age >= 50)
+                premium = premium * 0.15;
+
+            return (float)premium;
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SQT_Assignment;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(17,"rural", ExpectedResult = 0)]
+        [TestCase(18,"rural", ExpectedResult = 5)]
+        [TestCase(19,"rural", ExpectedResult = 5)]
+        [TestCase(29,"rural", ExpectedResult = 5)]
+        [TestCase(30,"rural", ExpectedResult = 5)]
+        [TestCase(31,"rural", ExpectedResult = 2.5)]
+        [TestCase(51,"rural", ExpectedResult = 0.375)]
+        [TestCase(17,"urban", ExpectedResult = 0)]
+        [TestCase(18,"urban", ExpectedResult = 6)]
+        [TestCase(19,"urban", ExpectedResult = 6)]
+        [TestCase(34,"urban", ExpectedResult = 5)]
+        [TestCase(35,"urban", ExpectedResult = 6)]
+        [TestCase(36,"urban", ExpectedResult = 5)]
+        [TestCase(51,"urban", ExpectedResult = 0.75)]
+        public float BoundaryTests(int a, string b)
+        {
+            InsuranceService insuranceService = new InsuranceService();
+            float result = insuranceService.CalcPremium(a, b);
+            return result;
+        }
+    }
+}
